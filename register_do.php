9e896243710e78c9966228fe23c226c98d3e13c8
Vento <vento@pfadi-nuenenen.ch>
2018-01-21 14:05:28 +0000

    Reimplement the mailsend-Funktion

---
+++ register_do.php
@@ -0,0 +1,102 @@
+<?php
+/*
+ * Copyright (C) 2010 Urban Suppiger, Pirmin Mattmann
+ *
+ * This file is part of eCamp.
+ *
+ * eCamp is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * eCamp is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with eCamp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+	include("./config.php");
+	include($lib_dir . "/mysql.php");
+	include($lib_dir . "/functions/mail.php");
+	db_connect();
+	
+	require_once( "./lib/recaptchalib.php" );
+
+	//	CHECK ALL INPUTS:
+	// ===================
+	$resp = recaptcha_check_answer ($GLOBALS['captcha_prv'], $_SERVER["REMOTE_ADDR"],
+									$_REQUEST["recaptcha_challenge_field"],
+									$_REQUEST["recaptcha_response_field"]	);
+
+	if (!$resp->is_valid)
+	{	header( 'location: register.php?msg=Bitte CAPTCHA richtig abschreiben!' );	die();	}
+	
+	if( $_REQUEST[ 'Login' ] == "" )
+	{	header( 'location: register.php?msg=eMail - Adresse muss angegeben werden!' );	die();	}
+	
+	if( !filter_var( $_REQUEST[ 'Login' ], FILTER_VALIDATE_EMAIL) )
+	{	header( 'location: register.php?msg=Eine gültige eMail - Adresse muss angegeben werden!' );	die();	}
+	
+	if( $_REQUEST[ 'Passwort1' ] == "" )
+	{	header( 'location: register.php?msg=Passwort muss angegeben werden!' );	die();	}
+	
+	if( $_REQUEST[ 'Passwort2' ] == "" )
+	{	header( 'location: register.php?msg=Wiederholung muss angegeben werden!' );	die();	}
+	
+	if( $_REQUEST[ 'firstname' ] == "" )
+	{	header( 'location: register.php?msg=Vorname muss angegeben werden!' );	die();	}
+	
+	if( $_REQUEST[ 'surname' ] == "" )
+	{	header( 'location: register.php?msg=Nachname muss angegeben werden!' );	die();	}
+	
+	$login 	= mysql_real_escape_string( $_REQUEST[ 'Login' ] );
+	$pw1	= md5( $_REQUEST[ 'Passwort1' ] );
+	$pw2	= md5( $_REQUEST[ 'Passwort2' ] );
+	
+	$scoutname 	= mysql_real_escape_string( $_REQUEST[ 'scoutname' ] );
+	$firstname 	= mysql_real_escape_string( $_REQUEST[ 'firstname' ] );
+	$surname 	= mysql_real_escape_string( $_REQUEST[ 'surname' ] );
+
+	if( $pw1 != $pw2 )
+	{	header( 'location: register.php?msg=Passwort unstimmig' );	die();	}
+
+	$query = "SELECT user.id FROM user WHERE user.mail = '" . $login . "'";
+	$result = mysql_query( $query );
+	
+	if( mysql_num_rows( $result ) )
+	{	header( 'location: register.php?msg=eMail-Adresse ist bereits registriert' );	die();	}
+	
+	//	INSERT NEW USER:
+	// ==================
+	$acode = md5( time() . $pw1 );
+
+	$query = "	INSERT INTO user ( `mail`, `pw`, `scoutname`, `firstname`, `surname`, `acode` )
+				VALUES ( '$login', '$pw1', '$scoutname', '$firstname', '$surname', '$acode' );";
+	mysql_query( $query );
+
+	$user_id = mysql_insert_id();
+
+	//	SEND MAIL FOR ACTIVATION:
+	// ===========================
+ 	$text = "eCamp - Willkommen \n\n
+Um dich bie eCamp einloggen zu können, musst du deinen Account aktivieren.
+Zu diesem Zweck musst du nachfolgendem Link folgen:
+\n\n
+" . $GLOBALS['base_uri'] . "activate.php?user_id=$user_id&login=$login&acode=$acode
+\n\n
+ ";
+
+	ecamp_send_mail($login, "eCamp - Willkommen", $text);
+ 	//mail( $login, "eCamp - Willkommen", $text, "From: eCamp Pfadi Luzern <ecamp@pfadiluzern.ch>" );
+	
+	/*
+	$text = urlencode( $text );
+ 	$subject = urlencode( "eCamp - Willkommen" );
+	fopen( "http://ecamp2.pfadiluzern.ch/mail.php?to=$login&subject=$subject&message=$text", "r" );
+	*/
+	
+	header( 'location: login.php?msg=Vor dem ersten Login muss der Account aktiviert werden. Dafür bitte Mailbox überprüfen.' );
+	die();
--- www/lib/functions/mail.php
+++ www/lib/functions/mail.php
@@ -26,10 +26,10 @@
 	require 'vendor/autoload.php';
 
 	function ecamp_send_mail($to, $subject, $body){
-		$mail = new PHPMailer(TRUE);
+		$mail = new PHPMailer(false);
 		try {
 			//Server settings
-			$mail->SMTPDebug = 2;                                 // Enable verbose debug output
+			$mail->SMTPDebug = 0;                                 // Enable verbose debug output
 			$mail->isSMTP();                                      // Set mailer to use SMTP
 			$mail->Host = $GLOBALS['smtp-config']['host'];        // Specify main and backup SMTP servers
 			$mail->SMTPAuth = true;                               // Enable SMTP authentication
