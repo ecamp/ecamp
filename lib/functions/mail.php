23a3ee70a75ba578fdd304f4c9be98c52089062c
Vento <vento@pfadi-nuenenen.ch>
2018-01-21 13:38:36 +0000

    Implement PHPMAILER

---
+++ lib/functions/mail.php
@@ -0,0 +1,67 @@
+<?php
+/*
+ * Copyright (C) 2010 Urban Suppiger, Pirmin Mattmann
+ *
+ * This file is part of eCamp.
+ *
+ * eCamp is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * eCamp is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with eCamp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+	// Import PHPMailer classes into the global namespace
+	// These must be at the top of your script, not inside a function
+	use PHPMailer;
+	use Exception;
+
+	require 'vendor/autoload.php';
+
+	function ecamp_send_mail($to, $subject, $body){
+		$mail = new PHPMailer(TRUE);
+		try {
+			//Server settings
+			$mail->SMTPDebug = 2;                                 // Enable verbose debug output
+			$mail->isSMTP();                                      // Set mailer to use SMTP
+			$mail->Host = $GLOBALS['smtp-config']['host'];        // Specify main and backup SMTP servers
+			$mail->SMTPAuth = true;                               // Enable SMTP authentication
+			$mail->Username = $GLOBALS['smtp-config']['username'];// SMTP username
+			$mail->Password = $GLOBALS['smtp-config']['password'];// SMTP password
+			$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+			//Custom connection options
+			//Note that these settings are INSECURE
+			$mail->SMTPOptions = array(
+				'ssl' => array(
+					'verify_peer' => false,
+					'verify_peer_name' => false,
+					'allow_self_signed' => true
+				),
+			);
+
+			$mail->Port = $GLOBALS['smtp-config']['port'];                                    // TCP port to connect to
+
+			//Recipients
+			$mail->setFrom('mailbot@ecamps.ch', 'eCamp Mailbot');
+			$mail->addAddress($to);     // Add a recipient
+
+			//Content
+			$mail->isHTML(true);                                  // Set email format to HTML
+			$mail->Subject = $subject;
+			$mail->Body    = $body;
+			$mail->AltBody = $body;
+
+			$mail->send();
+			echo 'Message has been sent';
+		} catch (Exception $e) {
+			echo 'Message could not be sent.';
+			echo 'Mailer Error: ' . $mail->ErrorInfo;
+		}
+	}
