0ed4cfc3b9bbd025cb942b688c1bf5f49905cc45
Vento-Nuenenen <35610192+Vento-Nuenenen@users.noreply.github.com>
2018-03-04 15:41:04 +0100

    OAuth Implementierung

--- www/login.php
+++ www/login.php
@@ -38,6 +38,7 @@
 	
 	$html->setEncoding('UTF-8');
 	$html->set('SHOW_MSG', false);
+    $html->set('LOGIN_TYPE', (($GLOBALS['auth'] == "midata") ? "MiData" : "Lokal"));
 	
 	session_start();
 
@@ -54,7 +55,105 @@
 		
 		// Verhindern von injection!!!
 		$_POST['Login'] = mysqli_real_escape_string($GLOBALS["___mysqli_ston"], $_POST['Login']);
-		
+
+        if($_POST['Login'])
+        {
+            // Get MiData Auth-Token
+            $curl = curl_init();
+            curl_setopt_array($curl, array(
+                CURLOPT_URL 			=> "https://db.scout.ch/users/sign_in.json?person[email]=".urlencode($_POST['Login'])."&person[password]=".urlencode($_POST['Passwort']),
+                CURLOPT_RETURNTRANSFER 	=> true,
+                CURLOPT_ENCODING 		=> "",
+                CURLOPT_HTTP_VERSION 	=> CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST 	=> "POST"
+            ));
+            $result	= json_decode(curl_exec($curl), true);
+            $href	= $result['people'][0]['href'];
+            $token	= $result['people'][0]['authentication_token'];
+
+            // Get details of person
+            curl_setopt_array($curl, array(
+                CURLOPT_URL 			=> $href."?user_email=".urlencode($_POST['Login'])."&user_token=".$token,
+                CURLOPT_CUSTOMREQUEST 	=> "GET"
+            ));
+            $result	= json_decode(curl_exec($curl), true);
+
+            if($result)
+            {
+                // Check if member of group
+                $groups = $result['linked']['groups'];
+
+                $i = $result['people'][0];
+
+                $id			= $i['id'];
+                $mail		= $i['email'];
+                $pw			= md5($_POST['Passwort']);
+                $scoutname	= $i['nickname'];
+                $firstname	= $i['first_name'];
+                $surname	= $i['last_name'];
+                $street		= (isset($i['address'])) ? $i['address'] : "NULL";
+                $zipcode	= (isset($i['zip_code'])) ? $i['zip_code'] : "NULL";
+                $city		= (isset($i['town'])) ? $i['town'] : "NULL";
+                $birthday	= (isset($i['birthday'])) ? $i['birthday'] : "NULL";
+                $ahvnr      = (isset($i['ahv_nummer'])) ? $i['ahv_nummer'] : "NULL";
+                $sex		= (isset($i['gender'])) ? (($i['gender'] == "m") ? 0 : 1) : "NULL";
+                $jspersnr	= (isset($i['j_s_number'])) ? $i['j_s_number'] : "NULL";
+                $admin		= 0;
+                $active		= 1;
+
+                $j = $result['linked'];
+
+                foreach ($j['phone_numbers'] as $phone_number){
+                    if ($phone_number['label'] == "Privat"){
+                        $priv_number = $phone_number['number'];
+                    }elseif ($phone_number['label'] == "Mobil"){
+                        $mobile_number = $phone_number['number'];
+                    }
+                }
+
+                // Format birthday
+                require_once( $GLOBALS['lib_dir'] . "/functions/date.php" );
+                if($birthday != "NULL")
+                {
+                    $d = date_create($birthday);
+                    $d = date_format($d, "d-m-Y");
+                    $date = new c_date;
+                    $date->setString($d);
+                    $birthday = $date->getValue();
+                }
+
+                // Check if already in db
+                $query = "SELECT * FROM user WHERE id=$id LIMIT 1;";
+                if(mysqli_num_rows(mysqli_query($GLOBALS['___mysqli_ston'],$query)))
+                {
+                    $query = "
+                        UPDATE user SET
+                        mail='$mail',pw='$pw',scoutname='$scoutname',firstname='$firstname',surname='$surname',street='$street',
+                        zipcode='$zipcode',city='$city',homenr='$priv_number',mobilnr='$mobile_number',birthday='$birthday',ahv='$ahvnr',sex=$sex,jspersnr='$jspersnr'
+                        WHERE id=$id;
+                    ";
+                }
+                else
+                {
+                    $query = "
+                        INSERT INTO user
+                        (id, mail, pw, scoutname, firstname, surname, street, zipcode, city, homenr, mobilenr, birthday, ahv, sex, jspersnr, admin, active)
+                        VALUES
+                        ($id, '$mail', '$pw', '$scoutname', '$firstname', '$surname', '$street', '$zipcode', '$city', $priv_number, $mobile_number, '$birthday', $ahvnr, $sex, '$jspersnr', $admin, $active)
+                    ";
+                }
+
+                mysqli_query($GLOBALS['___mysqli_ston'],$query);
+            }
+            else
+            {
+                $html->set('SHOW_MSG', true);
+                $html->set('MSG', "MiData Anmeldung fehlgeschlagen.");
+            }
+            curl_close($curl);
+
+        }
+
 		$query = "SELECT pw, id, scoutname, firstname, active, last_camp FROM user WHERE mail = '" . $_POST['Login'] . "' LIMIT 1";
 		$result = mysqli_query($GLOBALS["___mysqli_ston"], $query);
 		
--- www/public/skin/skin3/login.tpl
+++ www/public/skin/skin3/login.tpl
@@ -103,6 +103,7 @@
                 <table width="80%">
                     <tr><td colspan="2"><font class="title">eCamp - Login</font></td></tr>
                     <tr height="10"><td> </td></tr>
+					<select name="login_type" class="hidden"><option value="midata">MiData</option></select>
                     <tr><td>E-Mail:</td><td><input tabindex="1" name="Login" type="text" id="Login" /></td></tr>
                     <tr><td>Passwort:</td><td><input tabindex="2" name="Passwort" type="password" /></td></tr>
 					
