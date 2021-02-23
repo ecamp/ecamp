702d03d8e274fc2cbc8bccc0a868174a090d4185
Urban Suppiger <urban@suppiger.net>
2021-02-23 16:06:41 +0100

    upgrade TCPDF & FPDI to latest version

--- www/application/print/builder.php
+++ www/application/print/builder.php
@@ -18,114 +18,104 @@
  * along with eCamp.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-	if( !isset( $_REQUEST['item'] ) )
-	{	header( "location: index.php?app=print" );	die();	}
-	else
-	{
-		$items = $_REQUEST['item'];
-		$conf  = $_REQUEST['conf'];
-	}
+    if (!isset($_REQUEST['item'])) {
+        header("location: index.php?app=print");
+        die();
+    } else {
+        $items = $_REQUEST['item'];
+        $conf  = $_REQUEST['conf'];
+    }
 
-	ini_set("memory_limit","64M");
-	
-	require_once( 'class/data.php' );
-	require_once( 'class/build.php' );
+    
+    require_once('class/data.php');
+    require_once('class/build.php');
+    require_once('include/fpdi_addons.php');
+    
+    
+    $print_data = new print_data_class($_camp->id);
+    $print_build = new print_build_class($print_data);
+    
 
-	
-	require_once( 'tcpdf/tcpdf.php' );
-	require_once( 'tcpdf/tcpdf_addons.php' );
-	require_once( 'FPDI/fpdi.php' );
-	
-	
-	$print_data = new print_data_class( $_camp->id );	
-	$print_build = new print_build_class( $print_data );
-	
-	
-	
-	$pdf = new FPDI('P', 'mm', 'A4', true, 'UTF-8', false);
-	$pdf->SetAutoPageBreak(true);
-	
-	$pdf->SetAuthor( 'ecamp2.pfadiluzern.ch' );
-	$pdf->SetSubject( 'J&S - Programm' );
-	$pdf->SetTitle( 'J&S - Programm' );
+    $pdf = new Fpdi_Addons('P', 'mm', 'A4', true, 'UTF-8', false);
+    $pdf->SetAutoPageBreak(true);
+    
+    $pdf->SetAuthor('ecamp2.pfadiluzern.ch');
+    $pdf->SetSubject('J&S - Programm');
+    $pdf->SetTitle('J&S - Programm');
 
-	foreach( $items as $nr => $item )
-	{
-		if( $item == "title" )
-		{	$print_build->cover->build( $pdf );	}
-		
-		if( $item == "picasso" )
-		{
-			$print_build->picasso->set_orientation( $conf[$nr] );
-			$print_build->picasso->build( $pdf );
-		}
-		
-		if( $item == "allevents" )
-		{
-			if( $conf[$nr] == "true" )
-			{	$print_build->daylist->build( $pdf );	}
-			
-			foreach( $print_build->data->get_sorted_day() as $day )
-			{
-				$pdf->SetY( $print_build->day->build( $pdf, $day ) );
-				
-				foreach( $day->get_sorted_event_instance() as $event_instance )
-				{	$print_build->event->build( $pdf, $event_instance );	}
-			}
-		}
-		
-		if( $item == "event" )
-		{
-			$event_instance_id = $conf[$nr]['event_instance'];
-			
-			$_camp->event_instance( $event_instance_id ) || die( "error" );
-			
-			
-			if( $conf[$nr]['dayoverview'] == "true" )
-			{	$print_build->day->build( $pdf, $print_build->data->event_instance[ $event_instance_id ]->day );	}
-			else
-			{	$pdf->addPage('P', 'A4');	}
-			
-			
-			$print_build->data->event_instance[ $event_instance_id ]->day->gen_event_nr();
-			$print_build->event->build( $pdf, $print_build->data->event_instance[ $event_instance_id ] );
-		}
-		
-		if( $item == "toc" )
-		{	$print_build->toc->addTOC( $pdf );	}
+    foreach ($items as $nr => $item) {
+        if ($item == "title") {
+            $print_build->cover->build($pdf);
+        }
+        
+        if ($item == "picasso") {
+            $print_build->picasso->set_orientation($conf[$nr]);
+            $print_build->picasso->build($pdf);
+        }
+        
+        if ($item == "allevents") {
+            if ($conf[$nr] == "true") {
+                $print_build->daylist->build($pdf);
+            }
+            
+            foreach ($print_build->data->get_sorted_day() as $day) {
+                $pdf->SetY($print_build->day->build($pdf, $day));
+                
+                foreach ($day->get_sorted_event_instance() as $event_instance) {
+                    $print_build->event->build($pdf, $event_instance);
+                }
+            }
+        }
+        
+        if ($item == "event") {
+            $event_instance_id = $conf[$nr]['event_instance'];
+            
+            $_camp->event_instance($event_instance_id) || die("error");
+            
+            
+            if ($conf[$nr]['dayoverview'] == "true") {
+                $print_build->day->build($pdf, $print_build->data->event_instance[ $event_instance_id ]->day);
+            } else {
+                $pdf->addPage('P', 'A4');
+            }
+            
+            
+            $print_build->data->event_instance[ $event_instance_id ]->day->gen_event_nr();
+            $print_build->event->build($pdf, $print_build->data->event_instance[ $event_instance_id ]);
+        }
+        
+        if ($item == "toc") {
+            $print_build->toc->addTOC($pdf);
+        }
 
-		if( $item == "notes" )
-		{
-			for( $i = 1; $i <= $conf[$nr]; $i++){
-				$print_build->notes->build($pdf);
-			}
-		}
+        if ($item == "notes") {
+            for ($i = 1; $i <= $conf[$nr]; $i++) {
+                $print_build->notes->build($pdf);
+            }
+        }
 
-		if( $item == "pdf" )
-		{
-			$file = $_FILES[$conf[$nr]];
-			
-			$page_nr = $pdf->setSourceFile( $file['tmp_name'] );
-			
-			for( $i = 1; $i <= $page_nr; $i++ )
-			{
-				$tplidx = $pdf->ImportPage( $i );
-				$s = $pdf->getTemplatesize($tplidx); 
-				
-				$pdf->AddPage( 'P', array($s['w'], $s['h']) );
-				$pdf->useTemplate( $tplidx );
-			}
-			//$pdf->setPageFormat( 'A4', 'P' );
-		}
-	}
-	
-	
-	$print_build->toc->build( $pdf );
-	
-	
-	
-	$pdf->output( $_camp->short_name . ".pdf", 'I' );
+        if ($item == "pdf") {
+            $file = $_FILES[$conf[$nr]];
+            
+            $page_nr = $pdf->setSourceFile($file['tmp_name']);
+            
+            for ($i = 1; $i <= $page_nr; $i++) {
+                $tplidx = $pdf->ImportPage($i);
+                $s = $pdf->getTemplatesize($tplidx);
+                
+                $pdf->AddPage('P', array($s['w'], $s['h']));
+                $pdf->useTemplate($tplidx);
+            }
+            //$pdf->setPageFormat( 'A4', 'P' );
+        }
+    }
+    
+    
+    $print_build->toc->build($pdf);
+    
+    
+    
+    $pdf->output($_camp->short_name . ".pdf", 'I');
 
-	
-	die();
-?>
+    
+    die();
