da69860f9d56f4ceac7d7322ad6c5f8c960c535a
Urban Suppiger <urban@suppiger.net>
2021-02-28 13:50:39 +0100

    reduce memory_limit to 16MB (except for printing and XLS generating)

--- src/application/print/builder.php
+++ src/application/print/builder.php
@@ -26,7 +26,10 @@
         $conf  = $_REQUEST['conf'];
     }
 
-    
+    # increase memory limit for printing
+    # keep overall memor_limit low to allow for more FPM processes per server (high WEB_CONCURRENCY)
+    ini_set("memory_limit","80M");
+
     require_once('class/data.php');
     require_once('class/build.php');
     require_once('include/fpdi_addons.php');
